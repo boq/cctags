a68db2c1d1ceb4dda54f129ae97127bed55dd3de
Bartek Bok <bartek.bok@gmail.com>
2013-07-28 14:35:10 +0200

    Add TOC to library.html, fix split category

--- doc/generate-library.py
+++ doc/generate-library.py
@@ -1,4 +1,5 @@
 import json
+import cgi
 import sys
 import os
 import os.path as path
@@ -37,7 +38,13 @@ HTML_HEADER = """<!doctype html>
 <body>
 """
 
-CATEGORY_HEADER = "<h1>{category}</h1>\n"
+TOC_HEADER = '<h1 id="toc">Categories</h1>\n'
+
+CONTENTS_HEADER = '<h1 id="contents">Contents</h1>\n'
+
+CATEGORY_LINK = '<a href="#{id}">{category}</a>\n'
+
+CATEGORY_HEADER = '<h2 id="{id}">{category}</h2>\n'
 
 CATEGORY_NOTE = '<p class="note">{note}</p>\n'
 
@@ -48,7 +55,7 @@ ICON_TABLE_HEADER = """<table><thead>
 </thead><tbody>
 """
 
-ICON_TABLE_ENTRY = """<tr><td class="name">{name}</td><td style="color: #{color};">{icon}</td><td>{label}</td><td class="desc">{comment}</td></tr>
+ICON_TABLE_ENTRY = """<tr id="{id}"><td class="name">{name}</td><td style="color: #{color};">{icon}</td><td>{label}</td><td class="desc">{comment}</td></tr>
 """
 
 ICON_TABLE_ENTRY_CONTENTS = """<tr><th>Contents:</th><td colspan="3">{contents}</td></tr>
@@ -63,8 +70,13 @@ def do_work(input_path, output_path):
     with open(input_path, "r") as input, open(output_path, "wb") as output:
         output.write(HTML_HEADER)
         data = json.load(input)
+        output.write(TOC_HEADER)
         for category in data:
-            output.write(CATEGORY_HEADER.format(category = category['name']))
+             output.write(CATEGORY_LINK.format(category = category['name'], id = category['name']))
+        
+        output.write(CONTENTS_HEADER)
+        for category in data:
+            output.write(CATEGORY_HEADER.format(category = category['name'], id = category['name']))
             
             note = category.get("notes")
             if note:
@@ -78,13 +90,14 @@ def do_work(input_path, output_path):
             for tag in category['tags']:
                 tmp = dict()
                 tmp['name'] = tag['name']
+                tmp['id'] = tag['name']
                 tmp['color'] = tag.get('color', default_color)
-                tmp['icon'] = tag.get('icon', default_icon)
-                tmp['label'] = tag['label']
+                tmp['icon'] = cgi.escape(tag.get('icon', default_icon))
+                tmp['label'] = cgi.escape(tag['label'])
                 tmp['comment'] = tag.get('comment', "")
                 
                 output.write(ICON_TABLE_ENTRY.format(**tmp))
-                output.write(ICON_TABLE_ENTRY_CONTENTS.format(contents = tag['contents']))
+                output.write(ICON_TABLE_ENTRY_CONTENTS.format(contents = cgi.escape(tag['contents'])))
                 
                 description = tag.get('description')
                 if description:
